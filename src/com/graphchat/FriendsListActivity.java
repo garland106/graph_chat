bef48774d1438930028e5ee3ef6c3e08304048f9
Sy Adamowsky <syadamowsky@gmail.com>
2014-10-04 20:29:50 -0400

    FriendsListActivity edited, added ListView

--- src/com/graphchat/FriendsListActivity.java
+++ src/com/graphchat/FriendsListActivity.java
@@ -3,15 +3,21 @@ package com.graphchat;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ListView;
 
 public class FriendsListActivity extends Activity 
 {
+	private ListView friendsList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.signup_page);
+		setContentView(R.layout.friends_page);
+		friendsList = (ListView) findViewById(R.id.friendslv);
+		
 	}
 
+	
 }
