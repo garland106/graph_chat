cd008a3251c7990c391887dc5a093736dd87f6b5
Garland106 <garlandc@usc.edu>
2014-10-05 03:19:51 -0400

    parsing things:

--- src/com/graphchat/LoginPage.java
+++ src/com/graphchat/LoginPage.java
@@ -1,6 +1,7 @@
 package com.graphchat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -8,11 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.parse.LogInCallback;
 import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
+import com.parse.ParseObject;
+import com.utils.Message;
+import com.utils.ParseAPIUtils;
 
 public class LoginPage extends Activity
 {
@@ -36,6 +36,7 @@ public class LoginPage extends Activity
 		registerB.setOnClickListener(registerListener);
 		
 		Parse.initialize(this, "j2hPTdOqQufughrO83ZWMCFgXUratrMTugAv5XTs", "y4fgHJizh6s21DWWidrPgvpZjm4gOpfcgMHHv8g1");
+		
 	}
 	
 	/**
@@ -54,20 +55,9 @@ public class LoginPage extends Activity
 			}
 			else
 			{
-				ParseUser.logInInBackground(un, pw, new LogInCallback()
-				{
-					  public void done(ParseUser user, ParseException e) 
-					  {
-						  if (e == null) 
-						    {
-						    	Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
-						    } 
-						    else 
-						    {
-						    	Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_SHORT).show();
-						    }
-					  }
-					});
+				ParseAPIUtils.login(un, pw, getApplicationContext());
+				Intent intent = new Intent(LoginPage.this, ChatRoomActivity.class);
+				startActivity(intent);
 			}
 		}
 	};
@@ -85,25 +75,7 @@ public class LoginPage extends Activity
 			}
 			else
 			{
-				ParseUser user = new ParseUser();
-				user.setUsername(un);
-				user.setPassword(pw);
-				user.setEmail(un);
-				  
-				user.signUpInBackground(new SignUpCallback() 
-				{
-				  public void done(ParseException e) 
-				  {
-				    if (e == null) 
-				    {
-				    	Toast.makeText(getApplicationContext(), "Register Success", Toast.LENGTH_SHORT).show();
-				    } 
-				    else 
-				    {
-				    	Toast.makeText(getApplicationContext(), "Registration Failed", Toast.LENGTH_SHORT).show();
-				    }
-				  }
-				});
+				ParseAPIUtils.register(un, pw, getApplicationContext());
 			}
 		}
 	};
--- src/com/utils/Constants.java
+++ src/com/utils/Constants.java
@@ -2,5 +2,9 @@ package com.utils;
 
 public class Constants 
 {
-	
+	public static final String MESSAGES_KEY = "messages";
+	public static final String MESSAGE_CONTENT = "messagecontent";
+	public static final String USER_ID_KEY = "userId";
+	public static final String BODY_KEY = "body";
+	public static final int MAX_MESSAGES = 50;
 }
